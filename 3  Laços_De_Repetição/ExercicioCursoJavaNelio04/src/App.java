5ffe9683af1e0d1cba3d5b3d92b84b096133bd94
Kauan Araujo <araujokauan664@gmail.com>
2025-09-12 17:25:52 -0300

    Laços_De_Repetição

--- 3  Laços_De_Repetição/ExercicioCursoJavaNelio04/src/App.java
+++ 3  Laços_De_Repetição/ExercicioCursoJavaNelio04/src/App.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class App {  
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite um numero: ");
         int num = scanner.nextInt();
